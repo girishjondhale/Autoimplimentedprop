18eda0172126976d43b542a669db3a69b46edf4f
girishjondhale <girishjondhale1997@gmail.com>
2023-08-20 03:28:23 -0700

    Add project files.

---
+++ Autoimplimentedprop/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoimplimentedprop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.roll = 25;
+            student.name = "Abc";
+            student.percentage = 71;
+
+            Console.WriteLine(student.roll);
+
+        }
+
+    }
+}
---
+++ Autoimplimentedprop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoimplimentedprop
+{
+    public class Student
+    {
+        public int roll { get; set; }
+        public string name { get; set; }
+        public double percentage { get; set; }
+
+        public string PrintStudent()
+        {
+            return $"RollNo={roll},Name={name},Percentage={percentage}";
+        }
+    }
+}
